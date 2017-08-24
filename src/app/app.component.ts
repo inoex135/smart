5899eb0690112b7c7ee599527ab9dd4b3a113227
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-08-24 10:38:54 +0700

    Refactor login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
+import { UserProvider } from '../providers/user/user';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    userProvider: UserProvider
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      userProvider.populate();
     });
   }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,13 +8,16 @@ import {
 
 import { User } from "../../models/users";
 import { UserProvider } from '../../providers/user/user';
+import { HomePage } from '../home/home';
+import { TokenProvider } from '../../providers/token/token';
+import { ApiProvider } from '../../providers/api/api';
 
 /**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+* Generated class for the LoginPage page.
+*
+* See http://ionicframework.com/docs/components/#navigation for more info
+* on Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
@@ -25,41 +28,38 @@ import { UserProvider } from '../../providers/user/user';
 export class LoginPage {
   user = {} as User;
   error: any = false;
-
+  
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public userProvider: UserProvider,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    public tokenProvider: TokenProvider,
+    public apiProvider: ApiProvider
   ) {}
-
+  
   ionViewDidLoad() {
     console.log("ionViewDidLoad LoginPage");
   }
-
+  
   login(user: User) {
     let loader = this.loadingCtrl.create({ content: "loading....." });
     // mapping credential data
     let credentials = { user: user };
-
-    loader.present().then(() => {
-        this.userProvider
-          .attemptAuth(credentials)
-          .subscribe(
-          data => {
-            // console.log(data);
-            
-            loader.dismiss()
-          },
-            err => {
-              console.log(credentials);
-
-              loader.dismiss();
-            }
-          );
-      })
-      .catch(e => {
-        loader.dismiss();
-      });
+    
+    loader.present().then(() => { 
+      this.userProvider.attemptAuth(credentials).subscribe(
+        data => {
+          loader.dismiss()
+          this.navCtrl.setRoot(HomePage)
+        },
+        err => {
+          loader.dismiss();
+        }
+      );
+    })
+    .catch(e => {
+      loader.dismiss();
+    });
   }
 }
