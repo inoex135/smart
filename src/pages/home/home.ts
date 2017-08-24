b062000f8eb3af383566ec3f6e2f908160eeb07b
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-08-24 16:26:51 +0700

    Home Skeleton page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UserProvider } from '../../providers/user/user';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, public userProvider: UserProvider) { }
 
-  constructor(public navCtrl: NavController) {
-
+  logout() {
+    this.userProvider.purgeAuth();
+    this.navCtrl.setRoot(LoginPage);
   }
 
 }
