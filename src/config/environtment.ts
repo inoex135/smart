35d044e9798261939ee90d61d3439318befc987d
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-08-25 14:59:50 +0700

    Set api url for dev

--- src/config/environtment.ts
+++ src/config/environtment.ts
@@ -1,2 +1,2 @@
-export const ENV = { API_URL: "http://localhost:8000/api" };
-// export const ENV = { API_URL: "http://10.1.14.218:9000" };
+// export const ENV = { API_URL: "http://localhost:8000/api" };
+export const ENV = { API_URL: "http://10.1.14.218:9000/api" };
