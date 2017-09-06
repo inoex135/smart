23f8f36d6d39447652d034f1296583582d18d97c
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-09-06 17:13:36 +0700

    Add skeleton download file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,15 +3,25 @@ import { NavController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { LoginPage } from '../login/login';
 
+import { TokenProvider } from '../../providers/token/token';
+import { SuratProvider } from '../../providers/surat/surat';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, public userProvider: UserProvider) { }
+  constructor(
+    public navCtrl: NavController,
+    public userProvider: UserProvider,
+    public suratProvider: SuratProvider,
+  ) { }
+
   
   logout() {
     this.userProvider.purgeAuth();
     this.navCtrl.setRoot(LoginPage);
   }
+
+  goToSurat() { }
 }
--- src/providers/surat/surat.ts
+++ src/providers/surat/surat.ts
@@ -3,21 +3,47 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { ApiProvider } from '../api/api';
 
-/*
-  Generated class for the SuratProvider provider.
+import { FileTransfer, FileTransferObject } from "@ionic-native/file-transfer";
+import { FileOpener } from "@ionic-native/file-opener";
+import { File } from "@ionic-native/file";
+import { TokenProvider } from '../token/token';
+import { ENV } from '../../config/environtment';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
 @Injectable()
 export class SuratProvider {
+  constructor(
+    public http: Http,
+    public api: ApiProvider,
+    public transfer: FileTransfer,
+    public fileOpener: FileOpener,
+    public file: File,
+    public token: TokenProvider
+  ) {}
 
-  constructor(public http: Http, public api: ApiProvider) {
-    console.log('Hello SuratProvider Provider');
-  }
-  
-  getSumasTemplateExcel() {
-    return this.api.get('/surat/sumas/excel/template');
+  fileTransfer: FileTransferObject = this.transfer.create();
+
+  async getSumasTemplateExcel() {
+    const url = `${ENV.API_URL}/surat/sumas/excel/template`;
+    // const url = "http://www.gajotres.net/wp-content/uploads/2015/04/logo_radni.png";
+    
+    // File name only
+    var filename = url.split("/").pop();
+
+    let fileDirectory: string = this.file.dataDirectory + "1504670828948.xlsx";
+
+    return await this.fileTransfer.download(url, fileDirectory, true, this.getHeaders())
+      .then(entry => console.log(entry))
+      .catch(err => console.log(err));
   }
 
+  getHeaders() {
+    const options: Object = {};
+    let headers = {};
+    
+    headers["Authorization"] = "smartdjkn2017mobile";
+    headers["token"] = this.token.latestToken;
+    options["headers"] = headers;
+    
+    return options;
+  }
 }
