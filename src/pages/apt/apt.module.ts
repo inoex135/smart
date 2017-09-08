24e0cd9a9ce475ef40044bdf41cdda7bdc05e905
muhtarudin <muhtarudin@javan.co.id>
2017-09-08 14:52:52 +0700

    Add skeleton APT page

---
+++ src/pages/apt/apt.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AptPage } from './apt';
+
+@NgModule({
+  declarations: [
+    AptPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AptPage),
+  ],
+})
+export class AptPageModule {}
