a959e6582f2a908e737daea10cb20dc2ecc26e51
muhtarudin <muhtarudin@javan.co.id>
2017-09-11 17:09:12 +0700

    Add menu landing page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
-// import { UserProvider } from "../../providers/user/user";
+import { UserProvider } from "../../providers/user/user";
 import { LoginPage } from "../login/login";
+import { SuratPage } from "../surat/surat";
 
 @Component({
   selector: "page-home",
@@ -9,11 +10,16 @@ import { LoginPage } from "../login/login";
 })
 export class HomePage {
   constructor(
-    public navCtrl: NavController // public userProvider: UserProvider
+    public navCtrl: NavController,
+    public userProvider: UserProvider
   ) {}
 
   logout() {
-    // this.userProvider.purgeAuth();
-    this.navCtrl.setRoot(LoginPage);
+    this.userProvider.purgeAuth();
+    // this.navCtrl.setRoot(LoginPage);
+  }
+
+  goToPersuratan() {
+    this.navCtrl.push(SuratPage);
   }
 }
