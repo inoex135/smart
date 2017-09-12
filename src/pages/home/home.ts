40bd8ff825826fb5c898a222989d912e327dd08e
muhtarudin <muhtarudin@javan.co.id>
2017-09-12 11:38:36 +0700

    Add link to other page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { NavController } from "ionic-angular";
 import { UserProvider } from "../../providers/user/user";
 import { LoginPage } from "../login/login";
 import { SuratPage } from "../surat/surat";
+import { PersonalPageModule } from "../personal/personal.module";
+import { PersonalPage } from "../personal/personal";
+import { AptPage } from "../apt/apt";
 
 @Component({
   selector: "page-home",
@@ -14,12 +17,19 @@ export class HomePage {
     public userProvider: UserProvider
   ) {}
 
+  aptPage() {
+    this.navCtrl.push(AptPage);
+  }
   logout() {
     this.userProvider.purgeAuth();
-    // this.navCtrl.setRoot(LoginPage);
+    this.navCtrl.setRoot(LoginPage);
   }
 
-  goToPersuratan() {
+  persuratanPage() {
     this.navCtrl.push(SuratPage);
+  }
+
+  personalPage() {
+    this.navCtrl.push(PersonalPage);
   }
 }
