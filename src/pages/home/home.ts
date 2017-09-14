1583adf29484774268fd0327f20461f5cc185c19
muhtarudin <muhtarudin@javan.co.id>
2017-09-14 15:35:03 +0700

    Refactor login page using new template

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,6 @@ import { NavController } from "ionic-angular";
 import { UserProvider } from "../../providers/user/user";
 import { LoginPage } from "../login/login";
 import { SuratPage } from "../surat/surat";
-import { PersonalPageModule } from "../personal/personal.module";
 import { PersonalPage } from "../personal/personal";
 import { AptPage } from "../apt/apt";
 
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -14,6 +14,7 @@ export class LoginPage {
   user = {} as User;
   error: any = false;
   isLoading: boolean = false;
+  params: any = {};
 
   constructor(
     public navCtrl: NavController,
@@ -21,7 +22,44 @@ export class LoginPage {
     public userProvider: UserProvider,
     public tokenProvider: TokenProvider,
     public toastController: ToastController
-  ) {}
+  ) {
+    this.params.data = {
+      username: "Username",
+      password: "Password",
+      register: "Register",
+      login: "Login",
+      skip: "Skip",
+      logo: "assets/logo_keuangan.png",
+      backgroundImage: "icon-account",
+      iconAccount: "icon-key",
+      iconKey: "",
+      iconLock: "assets/images/background/loginlight.jpg"
+    };
+
+    this.params.events = {
+      onLogin: function(params) {
+        this.login(params);
+      },
+      onRegister: function(params) {
+        console.log("onRegister:" + JSON.stringify(params));
+      },
+      onSkip: function(params) {
+        console.log("onSkip:" + JSON.stringify(params));
+      },
+      onFacebook: function(params) {
+        console.log("onFacebook:" + JSON.stringify(params));
+      },
+      onTwitter: function(params) {
+        console.log("onTwitter:" + JSON.stringify(params));
+      },
+      onGoogle: function(params) {
+        console.log("onGoogle:" + JSON.stringify(params));
+      },
+      onPinterest: function(params) {
+        console.log("onPinterest:" + JSON.stringify(params));
+      }
+    };
+  }
 
   ionViewDidLoad() {}
 
