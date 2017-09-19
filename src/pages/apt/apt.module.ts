4e51310511e0f4c7772987249ce264f3a2c79cb3
muhtarudin <muhtarudin@javan.co.id>
2017-09-19 10:02:42 +0700

    Remove unused module

--- src/pages/apt/apt.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule } from "ionic-angular";
-import { AptPage } from "./apt";
-
-@NgModule({
-  declarations: [AptPage],
-  imports: [IonicPageModule.forChild(AptPage)]
-})
-export class AptPageModule {}
--- src/pages/surat/surat.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule } from "ionic-angular";
-import { SuratPage } from "./surat";
-
-@NgModule({
-  declarations: [SuratPage],
-  imports: [IonicPageModule.forChild(SuratPage)]
-})
-export class SuratPageModule {}
