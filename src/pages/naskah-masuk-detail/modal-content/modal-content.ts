478cffa873a30d52965e65088780c9970165a02b
muhtarudin <muhtarudin@javan.co.id>
2017-09-22 16:29:07 +0700

    Added modal content for naskah masuk detail

---
+++ src/pages/naskah-masuk-detail/modal-content/modal-content.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+import {
+  ModalController,
+  Platform,
+  NavParams,
+  ViewController
+} from "ionic-angular";
+@Component({
+  selector: "modal-content-page",
+  templateUrl: "modal-content.html"
+})
+export class ModalContentPage {
+  private actionType: String = "disposisi";
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    this.actionType = this.params.get("actionType");
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+}
