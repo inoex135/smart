9d82119e2807b4bec74b7390bca580981f67ad24
muhtarudin <muhtarudin@javan.co.id>
2017-09-22 10:32:23 +0700

    Add segment for detail naskah

--- src/pages/naskah-masuk-detail/naskah-masuk-detail.ts
+++ src/pages/naskah-masuk-detail/naskah-masuk-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { NavController, NavParams } from "ionic-angular";
+import { NavController, NavParams, ModalController } from "ionic-angular";
 import { NaskahDetailProvider } from "../../providers/naskah-detail/naskah-detail";
 @Component({
   selector: "page-naskah-masuk-detail",
@@ -7,11 +7,19 @@ import { NaskahDetailProvider } from "../../providers/naskah-detail/naskah-detai
 })
 export class NaskahMasukDetailPage {
   private detail: any = {};
+  private type: String = "detail"; //switch case for riwayat and detail segment
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private naskahProvider: NaskahDetailProvider
+    private naskahProvider: NaskahDetailProvider,
+    private modalCtrl: ModalController
   ) {}
+
+  modalAction(button: String) {
+    const modal = this.modalCtrl.create(null);
+
+    modal.present();
+  }
 
   ionViewDidLoad() {
     this.getDetailNaskah();
@@ -20,4 +28,10 @@ export class NaskahMasukDetailPage {
   getDetailNaskah() {
     this.detail = this.naskahProvider.getDetailNaskah();
   }
+
+  disposisi() {}
+
+  teruskan() {}
+
+  selesai() {}
 }
