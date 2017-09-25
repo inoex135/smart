e21c965375ae1dd27b306254c042443ca285ea83
muhtarudin <muhtarudin@javan.co.id>
2017-09-25 13:17:19 +0700

    Show jabatan dan petunjuk disposisi

--- src/components/persuratan/disposisi/disposisi.ts
+++ src/components/persuratan/disposisi/disposisi.ts
@@ -1,9 +1,17 @@
 import { Component, Input, EventEmitter, Output } from "@angular/core";
+import { PetunjukDisposisi } from "../../../constant/petunjuk-disposisi";
+import { Jabatan } from "../../../constant/jabatan";
 
 @Component({
   selector: "disposisi",
   templateUrl: "disposisi.html"
 })
 export class Disposisi {
-  constructor() {}
+  datas: any = {};
+  tanggalDisposisi: string = new Date().toISOString();
+  constructor() {
+    this.datas.petunjuk = PetunjukDisposisi.getPetunjuk();
+    this.datas.jabatan = Jabatan.getJabatan();
+    console.log(this.datas);
+  }
 }
