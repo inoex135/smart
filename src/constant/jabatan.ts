7870d8f943897ea607276e48c2a8bd0c714ff2e6
muhtarudin <muhtarudin@javan.co.id>
2017-09-25 13:15:37 +0700

    Add constant jabatan dan petunjuk disposisi

---
+++ src/constant/jabatan.ts
@@ -0,0 +1,10 @@
+export class Jabatan {
+  public static getJabatan() {
+    return [
+      "Kepala Seksi Perencanaan dan Pengembangan Sistem Aplikasi I",
+      "Kepala Seksi Perencanaan dan Pengembangan Sistem Aplikasi II",
+      "Kepala Seksi Perencanaan dan Pengembangan Sistem Aplikasi III",
+      "Kepala Seksi Integrasi Sistem Aplikasi"
+    ];
+  }
+}
---
+++ src/constant/petunjuk-disposisi.ts
@@ -0,0 +1,23 @@
+export class PetunjukDisposisi {
+  public static getPetunjuk(): Array<any> {
+    return [
+      "Setuju",
+      "Tolak",
+      "Selesaikan Sesuai Ketentuan yang Berlaku",
+      "Jawab",
+      "Perbaiki",
+      "Teliti & Pendapat",
+      "Teliti & Verifikasi",
+      "Sesuai Catatan",
+      "Untuk Perhatian",
+      "Untuk Diperhatikan",
+      "Edarkan",
+      "Bicarakan dengan Saya",
+      "Bicarakan Bersama dan Laporkan Hasilnya",
+      "Dijadwalkan",
+      "Simpan",
+      "Ingatkan",
+      "Harap dihadiri/ diwakili"
+    ];
+  }
+}
