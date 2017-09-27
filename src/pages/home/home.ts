35dd12d9e0fb5c87510d0f1a12594ced72e12b88
muhtarudin <muhtarudin@javan.co.id>
2017-09-27 10:50:32 +0700

    Refactor styling icon menu + added slides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { NavController, PopoverController } from "ionic-angular";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, PopoverController, Slides } from "ionic-angular";
 import { UserProvider } from "../../providers/user/user";
 import { LoginPage } from "../login/login";
 import { SuratPage } from "../surat/surat";
@@ -12,7 +12,39 @@ import { HomePopoverPage } from "../home-popover/home-popover";
   templateUrl: "home.html"
 })
 export class HomePage {
+  @ViewChild("slider") slider: Slides;
+
   public menus: Array<any> = [];
+  slides = [
+    {
+      title: "Dream's Adventure",
+      imageUrl:
+        "https://smart.javan.co.id/smart/themes/porto/assets/images/slide-bg.jpg",
+      songs: 2,
+      private: false
+    },
+    {
+      title: "For the Weekend",
+      imageUrl:
+        "https://smart.javan.co.id/smart/themes/porto/assets/images/slide-bg.jpg",
+      songs: 4,
+      private: false
+    },
+    {
+      title: "Family Time",
+      imageUrl:
+        "https://smart.javan.co.id/smart/themes/porto/assets/images/slide-bg.jpg",
+      songs: 5,
+      private: true
+    },
+    {
+      title: "My Trip",
+      imageUrl:
+        "https://smart.javan.co.id/smart/themes/porto/assets/images/slide-bg.jpg",
+      songs: 12,
+      private: true
+    }
+  ];
 
   constructor(
     public navCtrl: NavController,
@@ -30,15 +62,15 @@ export class HomePage {
         title: "Persuratan",
         icon: "mail",
         component: SuratPage,
-        color: "blue"
+        color: "bluesky"
       },
       {
         title: "Personal",
         icon: "person",
         component: PersonalPage,
-        color: "orange"
+        color: "blue"
       },
-      { title: "APT", icon: "laptop", component: AptPage, color: "green" }
+      { title: "APT", icon: "laptop", component: AptPage, color: "orange" }
     ];
 
     return this.menus;
