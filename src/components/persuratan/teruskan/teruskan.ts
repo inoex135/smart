4cdff6c37bc1a9bfc6c0a0e7edcad98be7e6111a
muhtarudin <muhtarudin@javan.co.id>
2017-10-12 11:03:45 +0700

    Refactor teruskan untuk naskah

--- src/components/persuratan/teruskan/teruskan.ts
+++ src/components/persuratan/teruskan/teruskan.ts
@@ -1,14 +1,27 @@
 import { Component } from "@angular/core";
+import { SuratTeruskan } from "../../../models/surat-teruskan";
+import { LoadingController, NavController } from "ionic-angular";
 
 @Component({
   selector: "teruskan",
   templateUrl: "teruskan.html"
 })
 export class Teruskan {
-  public surat: string;
-  public tujuan: string;
-  public catatan: string;
-  constructor() {}
+  surat: SuratTeruskan = {
+    tujuan: "",
+    alasan: "",
+    catatan: ""
+  };
 
-  teruskan() {}
+  constructor(public loading: LoadingController, public nav: NavController) {}
+
+  teruskan() {
+    const loading = this.loading.create({
+      content: "Please wait..."
+    });
+
+    loading.present();
+    console.log(this.surat);
+    loading.dismiss();
+  }
 }
