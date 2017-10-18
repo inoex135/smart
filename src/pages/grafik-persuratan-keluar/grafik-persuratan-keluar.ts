5b281e1544866c82270ae68c447e6dbb037a1811
muhtarudin <muhtarudin@javan.co.id>
2017-10-18 10:46:30 +0700

    Added funnel icon for filter + added dummy data for grafik-naskah-keluar component

--- src/pages/grafik-persuratan-keluar/grafik-persuratan-keluar.ts
+++ src/pages/grafik-persuratan-keluar/grafik-persuratan-keluar.ts
@@ -1,27 +1,18 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
+import * as moment from "moment";
 
 @Component({
   selector: "page-grafik-persuratan-keluar",
   templateUrl: "grafik-persuratan-keluar.html"
 })
 export class GrafikPersuratanKeluarPage {
-  public barChartLabels: string[] = [
-    "2006",
-    "2007",
-    "2008",
-    "2009",
-    "2010",
-    "2011",
-    "2012"
-  ];
+  public barChartLabels: string[] = moment.months();
 
   public barChartData: any[] = [
-    { data: [1, 2, 3, 4, 5, 6, 7], label: "Selesai" },
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Belum Proses" },
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Disposisi" },
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Teruskan" },
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Naskah Belum Terima" },
-    { data: [65, 59, 80, 81, 56, 55, 40], label: "Naskah Diterima" }
+    { data: [1, 2, 3, 4, 5, 6, 7], label: "Antrian" },
+    { data: [65, 59, 80, 81, 56, 55, 40], label: "Terkirim" }
   ];
+
+  constructor() {}
 }
