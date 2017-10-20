ecd7cc8d9f980116fa91d66388969b81cc4657c2
muhtarudin <muhtarudin@javan.co.id>
2017-10-20 16:17:44 +0700

    Remove unused component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,39 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { IonicModule } from "ionic-angular";
 import { LoginLayout1 } from "./login/layout-1/login-layout-1";
 
-import { IonicModule } from "ionic-angular";
-import { TabsLayout1 } from "./tabs/layout-1/tabs-layout-1";
-import { TabsLayout2 } from "./tabs/layout-2/tabs-layout-2";
-import { TabsLayout3 } from "./tabs/layout-3/tabs-layout-3";
-import { GoogleCardLayout1 } from "./google-card/google-card-layout-1";
 import { Disposisi } from "./persuratan/disposisi/disposisi";
 import { Teruskan } from "./persuratan/teruskan/teruskan";
 import { Selesai } from "./persuratan/selesai/selesai";
-import { ChartSuratComponent } from "./chart-surat/chart-surat";
-import { ChartsModule } from "ng2-charts/charts/charts";
-import "../../node_modules/chart.js/dist/Chart.bundle.min.js";
 @NgModule({
-  declarations: [
-    LoginLayout1,
-    TabsLayout1,
-    TabsLayout2,
-    TabsLayout3,
-    GoogleCardLayout1,
-    Disposisi,
-    Teruskan,
-    Selesai,
-    ChartSuratComponent
-  ],
-  imports: [IonicModule, ChartsModule],
-  exports: [
-    LoginLayout1,
-    GoogleCardLayout1,
-    TabsLayout1,
-    Disposisi,
-    Teruskan,
-    Selesai,
-    ChartSuratComponent
-  ],
+  declarations: [LoginLayout1, Disposisi, Teruskan, Selesai],
+  imports: [IonicModule],
+  exports: [LoginLayout1, Disposisi, Teruskan, Selesai],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ComponentsModule {}
