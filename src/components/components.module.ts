20979adb0de4f97074be40c4d18e91a838fb9f58
muhtarudin <muhtarudin@javan.co.id>
2017-10-30 13:44:14 +0700

    Added riwayat component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,10 +5,11 @@ import { LoginLayout1 } from "./login/layout-1/login-layout-1";
 import { Disposisi } from "./persuratan/disposisi/disposisi";
 import { Teruskan } from "./persuratan/teruskan/teruskan";
 import { Selesai } from "./persuratan/selesai/selesai";
+import { RiwayatComponent } from "./persuratan/riwayat/riwayat-component";
 @NgModule({
-  declarations: [LoginLayout1, Disposisi, Teruskan, Selesai],
+  declarations: [LoginLayout1, Disposisi, Teruskan, Selesai, RiwayatComponent],
   imports: [IonicModule],
-  exports: [LoginLayout1, Disposisi, Teruskan, Selesai],
+  exports: [LoginLayout1, Disposisi, Teruskan, Selesai, RiwayatComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ComponentsModule {}
---
+++ src/components/persuratan/riwayat/riwayat-component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "riwayat-component",
+  templateUrl: "riwayat-component.html"
+})
+export class RiwayatComponent {}
