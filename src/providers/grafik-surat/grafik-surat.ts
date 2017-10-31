c6422705e099ebfe5a5faa82df26f504d5956122
muhtarudin <muhtarudin@javan.co.id>
2017-10-31 15:50:05 +0700

    Refactor grafik provider

--- src/providers/grafik-surat/grafik-surat.ts
+++ src/providers/grafik-surat/grafik-surat.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ApiProvider } from "../api/api";
+import * as moment from "moment";
 
 @Injectable()
 export class GrafikSuratProvider {
@@ -7,6 +8,51 @@ export class GrafikSuratProvider {
   url: string = "/surat/grafik-sumas/umum";
 
   constructor(private api: ApiProvider) {}
+
+  paramsStartAndEnd() {
+    const date = {
+      startTime: moment()
+        .startOf("year")
+        .format("DD-MM-YYYY"),
+      endTime: moment()
+        .endOf("year")
+        .format("DD-MM-YYYY")
+    };
+
+    return date;
+  }
+
+  // convert params to new format
+  convertParam(param: any) {
+    return moment(param).format("DD-MM-YYYY");
+  }
+
+  // return params date with new format
+  filterParams(params: any) {
+    const newDate = {
+      startTime: this.convertParam(params.startTime),
+      endTime: this.convertParam(params.endTime)
+    };
+
+    return newDate;
+  }
+
+  chartData(series: any) {
+    const chartData = {
+      chart: {
+        type: "column"
+      },
+      title: {
+        text: "Info Umum Naskah Masuk"
+      },
+      xAxis: {
+        categories: moment.months()
+      },
+      series: series
+    };
+
+    return chartData;
+  }
 
   getSumasData() {
     return this.api.get(this.url).map(res => this.mappingDataSumas(res));
