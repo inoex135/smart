8f6c726fe42bb13622302b5ce69de50bd2622a06
muhtarudin <muhtarudin@javan.co.id>
2017-11-03 16:16:58 +0700

    Added riwayat component styling

--- src/components/persuratan/riwayat/riwayat-component.ts
+++ src/components/persuratan/riwayat/riwayat-component.ts
@@ -1,7 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "riwayat-component",
   templateUrl: "riwayat-component.html"
 })
-export class RiwayatComponent {}
+export class RiwayatComponent {
+  naskah: object = {
+    tanggal: "",
+    perihal: "",
+    jenis: "",
+    asalPengiriman: "",
+    sifat: "",
+    riwayat: []
+  };
+
+  ionViewDidLoad() {}
+}
