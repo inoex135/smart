d3750b71ec46434707cefceb2470fa681f8072cb
muhtarudin <muhtarudin@javan.co.id>
2017-11-06 10:31:22 +0700

    Refactor tampilan riwayat + naskah action

--- src/components/timeline/timeline.ts
+++ src/components/timeline/timeline.ts
@@ -9,25 +9,3 @@ export class TimelineComponent {
 
   constructor() {}
 }
-
-@Component({
-  selector: "timeline-item",
-  template: "<ng-content></ng-content>"
-})
-export class TimelineItemComponent {
-  @Input("items") items = [];
-  constructor() {}
-}
-
-@Component({
-  selector: "timeline-time",
-  template: `
-    <span>31-December-2018 - Disposisi</span>
-    <span>AM-4641/KN/2016</span>
-    <span>{{time?.title}}</span>
-    <span>{{time?.title}}</span>
-    `
-})
-export class TimelineTimeComponent {
-  @Input("time") time = {};
-}
--- src/pages/naskah-detail-action/naskah-detail-action.ts
+++ src/pages/naskah-detail-action/naskah-detail-action.ts
@@ -7,7 +7,29 @@ import { NavController, NavParams } from "ionic-angular";
 })
 export class NaskahDetailActionPage {
   actionData: object = {};
-
+  items = [
+    {
+      title: "Courgette daikon",
+      content:
+        "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+      icon: "calendar",
+      time: { subtitle: "4/16/2013", title: "21:30" }
+    },
+    {
+      title: "Courgette daikon",
+      content:
+        "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+      icon: "calendar",
+      time: { subtitle: "January", title: "29" }
+    },
+    {
+      title: "Courgette daikon",
+      content:
+        "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+      icon: "calendar",
+      time: { title: "Short Text" }
+    }
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
