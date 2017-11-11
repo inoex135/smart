07b448d9087fc3b2e2b45f830c1546311f18cf9e
muhtarudin <muhtarudin@javan.co.id>
2017-11-11 16:07:30 +0700

    Show data dummy in page

--- src/pages/personal-agenda-detail/personal-agenda-detail.ts
+++ src/pages/personal-agenda-detail/personal-agenda-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { PersonalAgendaDetailProvider } from "../../providers/personal-agenda-detail/personal-agenda-detail";
 
 @IonicPage()
 @Component({
@@ -7,9 +8,18 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "personal-agenda-detail.html"
 })
 export class PersonalAgendaDetailPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  detailAgenda: any = [];
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private agendaProvider: PersonalAgendaDetailProvider
+  ) {}
 
   ionViewDidLoad() {
-    console.log("ionViewDidLoad PersonalAgendaDetailPage");
+    this.getDetailAgenda();
+  }
+
+  getDetailAgenda() {
+    this.detailAgenda = this.agendaProvider.getDetailAgenda();
   }
 }
