de5cd5167af8914afa6e3746cfce4e0b0db7f491
muhtarudin <muhtarudin@javan.co.id>
2017-11-11 15:09:38 +0700

    Added dummy data for timeline agenda

---
+++ src/dummy/timeline.dummy.ts
@@ -0,0 +1,55 @@
+export class TimelineDummy {
+  static items() {
+    const items = [
+      {
+        title: "6 Maret 2017 - Disposisi",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { subtitle: "4/16/2013", title: "21:30" }
+      },
+      {
+        title: "6 Maret 2017 - Disposisi",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { subtitle: "January", title: "29" }
+      },
+      {
+        title: "6 Maret 2017 - Disposisi",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { title: "Short Text" }
+      }
+    ];
+    return items;
+  }
+
+  static agenda() {
+    const items = [
+      {
+        title: "6 Desember 2017",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { subtitle: "4/16/2013", title: "21:30" }
+      },
+      {
+        title: "6 Maret 2017 - Disposisi",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { subtitle: "January", title: "29" }
+      },
+      {
+        title: "6 Maret 2017 - Disposisi",
+        content:
+          "Parsley amaranth tigernut silver beet maize fennel spinach. Ricebean black-eyed pea maize scallion green bean spinach cabbage jícama bell pepper carrot onion corn plantain garbanzo. Sierra leone bologi komatsuna celery peanut swiss chard silver beet squash dandelion maize chicory burdock tatsoi dulse radish wakame beetroot.",
+        icon: "calendar",
+        time: { title: "Short Text" }
+      }
+    ];
+    return items;
+  }
+}
