5fc675d2a066e1ff77a6443b74db90a09b7f271d
muhtarudin <muhtarudin@javan.co.id>
2017-11-14 15:33:45 +0700

    Added pemberitahuan component

---
+++ src/components/pemberitahuan/pemberitahuan.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from "@angular/core";
+import * as moment from "moment-timezone";
+import { NavController } from "ionic-angular";
+
+@Component({
+  selector: "pemberitahuan",
+  templateUrl: "pemberitahuan.html"
+})
+export class PemberitahuanComponent {
+  constructor(private navCtrl: NavController) {}
+  @Input() redirectComponent: any = "";
+  ionViewDidLoad() {
+    this.now();
+  }
+
+  now() {
+    return moment.tz("Asia/Jakarta").format("HH:mm");
+  }
+
+  redirectTo() {
+    this.navCtrl.push(this.redirectComponent);
+  }
+}
