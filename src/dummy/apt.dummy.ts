8a576655864c55b35b2ad0035f7f60f8a48e2c48
muhtarudin <muhtarudin@javan.co.id>
2017-11-15 13:29:38 +0700

    Added dummy apt

---
+++ src/dummy/apt.dummy.ts
@@ -0,0 +1,23 @@
+export class AptDummy {
+  static getApt() {
+    const data = [
+      {
+        id: 1,
+        pelayanan: "Pengelolaan Kekayaan Negara",
+        sub_pelayanan:
+          "Penetapan Status Penggunaan Barang Milik Negara Selain Tanah Dan/Atau Bangunan",
+        tanggal_naskah: "04 Oktober 2017",
+        nomorPermohonan: "BBGSt244221"
+      },
+      {
+        id: 1,
+        pelayanan: "Pengelolaan Kekayaan Negara",
+        sub_pelayanan:
+          "Penetapan Status Penggunaan Barang Milik Negara Selain Tanah Dan/Atau Bangunan",
+        tanggal_naskah: "04 Oktober 2017",
+        nomorPermohonan: "Ba-90/WKN-211/2017"
+      }
+    ];
+    return data;
+  }
+}
