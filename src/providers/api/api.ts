f3fe4b4ab26f7b36f33e48828548669ed8838d7d
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-11-21 20:05:35 +0700

    Fixed case sensitive error observable import

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpHeaders, HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs/Observable";
 import { map, catchError } from "rxjs/operators";
-import { ErrorObservable } from "rxjs/observable/errorObservable";
+import { ErrorObservable } from "rxjs/observable/ErrorObservable";
 
 import { ENV } from "../../config/environment";
 import { TokenProvider } from "../token/token";
