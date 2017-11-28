9118b5a447c69fca7e690f9afaf171443baecf78
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-11-28 08:43:55 +0700

    Added riwayat naskah

--- src/components/persuratan/riwayat/riwayat-component.ts
+++ src/components/persuratan/riwayat/riwayat-component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { NaskahMasukProvider } from "../../../providers/naskah-masuk/naskah-masuk";
+import { NavParams } from "ionic-angular";
+import { TimelineType } from "../../../constant/TimelineType";
+import { TimelineDummy } from "../../../dummy/timeline.dummy";
 
 @Component({
   selector: "riwayat-component",
@@ -6,13 +10,38 @@ import { Component } from "@angular/core";
 })
 export class RiwayatComponent {
   naskah: object = {
-    tanggal: "",
+    asal_pengirim: "",
+    jenis_naskah: "",
+    lampiran: "",
+    nama_pengirim: "",
+    tanggal_naskah: "",
+    sifat_naskah: "",
     perihal: "",
-    jenis: "",
-    asalPengiriman: "",
-    sifat: "",
-    riwayat: []
+    riwayat: [],
+    unit_pengirim_naskah: ""
   };
 
-  ionViewDidLoad() {}
+  naskahId: number;
+  type: string;
+
+  constructor(
+    public naskahProvider: NaskahMasukProvider,
+    private navParams: NavParams
+  ) {
+    this.init();
+    this.getRiwayatNaskah(this.naskahId);
+  }
+
+  init() {
+    this.naskahId = this.navParams.get("naskahId");
+    this.type = TimelineType.RIWAYAT;
+  }
+  getRiwayatNaskah(naskahId: number) {
+    this.naskahProvider.riwayatNaskah(naskahId).subscribe(
+      res => {
+        this.naskah = res;
+      },
+      err => {}
+    );
+  }
 }
