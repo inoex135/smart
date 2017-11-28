c2a3c8e8d599b2626a870d434e3a6ffa147503ff
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-11-28 10:21:46 +0700

    Show files di page detail naskah

--- src/components/naskah-modal-download/naskah-modal-download.ts
+++ src/components/naskah-modal-download/naskah-modal-download.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component, Output, EventEmitter, Input } from "@angular/core";
 
 @Component({
   selector: "naskah-modal-download",
@@ -6,7 +6,7 @@ import { Component, Output, EventEmitter } from "@angular/core";
 })
 export class NaskahModalDownloadComponent {
   show: boolean = false;
-
+  @Input() files: Array<any> = [];
   @Output() downloadFile: EventEmitter<any> = new EventEmitter();
 
   constructor() {}
