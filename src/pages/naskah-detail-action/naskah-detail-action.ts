2b267f316cc9cde75b933f565413965404539ce3
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-11-28 08:44:08 +0700

    Remove unused code for component

--- src/pages/naskah-detail-action/naskah-detail-action.ts
+++ src/pages/naskah-detail-action/naskah-detail-action.ts
@@ -1,7 +1,5 @@
 import { Component } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
-import { TimelineDummy } from "../../dummy/timeline.dummy";
-import { TimelineType } from "../../constant/TimelineType";
 
 @Component({
   selector: "page-naskah-detail-action",
@@ -9,12 +7,8 @@ import { TimelineType } from "../../constant/TimelineType";
 })
 export class NaskahDetailActionPage {
   actionData: object = {};
-  items = TimelineDummy.items();
-  type: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.type = TimelineType.RIWAYAT;
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     this.actionData = this.navParams.get("actionData");
