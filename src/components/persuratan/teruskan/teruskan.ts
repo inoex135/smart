996c94bd72c661bbaac19cd54df8ad167fd8062d
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-12-01 12:42:01 +0700

    Added skeleton autocomplete teruskan

--- src/components/persuratan/teruskan/teruskan.ts
+++ src/components/persuratan/teruskan/teruskan.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { SuratTeruskan } from "../../../models/surat-teruskan";
 
 import { NaskahMasukProvider } from "../../../providers/naskah-masuk/naskah-masuk";
 
 import { NavController, NavParams, LoadingController } from "ionic-angular";
-
+import { MasterUnitProvider } from "../../../providers/master-unit/master-unit";
+import { AutoCompleteComponent } from "ionic2-auto-complete";
 
 @Component({
   selector: "teruskan",
@@ -17,12 +18,13 @@ export class Teruskan {
     alasan: "",
     catatan: ""
   };
-
+  @ViewChild("searchbar") searchbar: AutoCompleteComponent;
   constructor(
     public loading: LoadingController,
     public nav: NavController,
     public navParams: NavParams,
-    public naskahProvider: NaskahMasukProvider
+    public naskahProvider: NaskahMasukProvider,
+    private masterUnit: MasterUnitProvider
   ) {
     this.naskah.id = this.navParams.get("naskahId");
   }
@@ -37,13 +39,15 @@ export class Teruskan {
     // save data naskah untuk diteruskan
     this.naskahProvider.teruskan(this.naskah).subscribe(
       res => {
-        console.log(res);
         loading.dismiss();
       },
       err => {
-        console.log(err);
         loading.dismiss();
       }
     );
+  }
+
+  addData(data: Array<any>, item: any) {
+    // data.push(item);
   }
 }
