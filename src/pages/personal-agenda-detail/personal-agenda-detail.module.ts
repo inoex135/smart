1fb09a9e99746e702edab7ac42f54318adea4da9
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-12-05 02:59:53 +0700

    Added uraian, lokasi, jam dan tanggal di detail agenda personal

--- src/pages/personal-agenda-detail/personal-agenda-detail.module.ts
+++ src/pages/personal-agenda-detail/personal-agenda-detail.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { PersonalAgendaDetailPage } from "./personal-agenda-detail";
+import { PipesModule } from "../../pipes/pipes.module";
 
 @NgModule({
   declarations: [PersonalAgendaDetailPage],
-  imports: [IonicPageModule.forChild(PersonalAgendaDetailPage)]
+  imports: [IonicPageModule.forChild(PersonalAgendaDetailPage), PipesModule]
 })
 export class PersonalAgendaDetailPageModule {}
