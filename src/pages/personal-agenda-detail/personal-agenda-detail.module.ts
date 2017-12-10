a41573a23bd5c30d60812d7b375d9fa8340880a9
muhtarudin.siregar <muhtarudin@javan.co.id>
2017-12-10 19:24:22 +0700

    Fixing view agenda error 500

--- src/pages/personal-agenda-detail/personal-agenda-detail.module.ts
+++ src/pages/personal-agenda-detail/personal-agenda-detail.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { PersonalAgendaDetailPage } from "./personal-agenda-detail";
 import { PipesModule } from "../../pipes/pipes.module";
+import { ComponentsModule } from "../../components/components.module";
 
 @NgModule({
   declarations: [PersonalAgendaDetailPage],
-  imports: [IonicPageModule.forChild(PersonalAgendaDetailPage), PipesModule]
+  imports: [
+    IonicPageModule.forChild(PersonalAgendaDetailPage),
+    PipesModule,
+    ComponentsModule
+  ]
 })
 export class PersonalAgendaDetailPageModule {}
