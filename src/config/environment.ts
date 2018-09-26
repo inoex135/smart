14c767583df37a5bd441cfe9f06c32145275607f
indra sakti <indra@javan.co.id>
2018-09-26 09:19:12 +0700

    remove dev=true di environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,6 @@
-export const ENV = { API_URL: "https://layanandjkn.kemenkeu.go.id/api/api", DEV: true };
+export const ENV = { API_URL: "https://layanandjkn.kemenkeu.go.id/api/api"
+//, DEV: true 
+};
 // export const ENV = { 
 //     API_URL: "http://api.smart.javan.co.id/api",
 //     DEV: true
