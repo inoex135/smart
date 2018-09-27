9361d89c1732cd0627d6b0585403bc57babaa3f1
Agung Aldhiyat <agung.aldhiyat@gmail.com>
2018-09-27 10:38:54 +0700

    commit

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,5 @@
-export const ENV = { API_URL: "https://layanandjkn.kemenkeu.go.id/api/api", DEV: true };
+export const ENV = { API_URL: "http://smart2017.djkn.or.id/api/api", DEV: true };
+//export const ENV = { API_URL: "https://layanandjkn.kemenkeu.go.id/api/api", DEV: true };
 // export const ENV = { 
 //     API_URL: "http://api.smart.javan.co.id/api",
 //     DEV: true
