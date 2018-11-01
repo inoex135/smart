26b783480c1f9473432d8c962fe1d7405cda08bc
Ricky .D <ricky@javan.co.id>
2018-11-01 15:03:24 +0700

    update notification by force.

--- src/components/notification-bell/notification-bell.ts
+++ src/components/notification-bell/notification-bell.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from "@angular/core";
-import { App } from "ionic-angular";
+import { Component, Input, EventEmitter, Output } from "@angular/core";
 import { HomeProvider } from "../../providers/home/home";
 import { LogUtil } from "../../utils/logutil";
+import { Flags } from "@ionic-native/file";
 
 @Component({
   selector: "notification-bell",
@@ -11,29 +11,48 @@ export class NotificationBell {
 
   static TAG:string = "NotificationBell"
 
-  constructor(private app: App, private homeProvider: HomeProvider) {}
-
   @Input() notificationType: string = "";
 
   totalNotification:number = 0
 
-  ionViewWillEnter() {
-    LogUtil.d(NotificationBell.TAG, "view enter")
-    this.homeProvider.getTotalNotication().subscribe(
-      result => {
-        if (result) {
-          this.totalNotification = result.notification_apt + result.notification_personal + result.notification_persuratan
-          LogUtil.d(NotificationBell.TAG, "count total notifications: " + this.totalNotification)
+  isUpdating:boolean = false
+
+  constructor(private homeProvider: HomeProvider) {
+    this.updateNotification()
+  }
+
+  setTotalNotification(result:any) {
+    if (result) {
+      this.totalNotification = result.notification_apt + result.notification_personal + result.notification_persuratan
+      LogUtil.d(NotificationBell.TAG, "count total notifications: " + this.totalNotification)
+    }
+  }
+
+  updateNotification() {
+    if (!this.isUpdating) {
+      this.isUpdating = true
+      LogUtil.d(NotificationBell.TAG, "updating notification...")
+      this.homeProvider.getTotalNotication().subscribe(
+        res => {
+          this.setTotalNotification(res)
+          this.isUpdating = false
+        },
+        error => {
+          this.isUpdating = false
+          LogUtil.d(NotificationBell.TAG, "error")
         }
-      },
-      error => {
-        LogUtil.d(NotificationBell.TAG, "error")
-      }
-    )
+      )
+    } else {
+      LogUtil.d(NotificationBell.TAG, "already in updating state..")
+    }
   }
 
   isNotificationExist():boolean {
     return this.totalNotification > 0
+  }
+
+  redirectTo() {
+    console.log("redirect to")
   }
   
 }
