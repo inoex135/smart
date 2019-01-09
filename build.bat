b7466780a70f15f65c4601f4e7fe27b941752acb
Ricky .D <ricky@javan.co.id>
2019-01-09 09:57:28 +0700

    manually replace value DEV to true|false.

--- build.bat
+++ build.bat
@@ -4,5 +4,7 @@ echo remove older unsigned apk
 del ./platforms/android/build/outputs/apk/android-release-unsigned.apk
 echo remove older signed apk
 del .\smart.apk
+echo disabled log developement
+(Get-Content .\src\config\environment.ts) -replace '(DEV:)\s(true|false)', 'DEV: false' | Out-File -encoding UTF8 .\src\config\environment.ts
 echo build release
 ionic cordova build android --release --verbose
